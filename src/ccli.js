7583c157c563716356448ea9fe86e87d6597276f
Herman <herman@templ.io>
2022-03-10 18:01:21 +0100

    added version

--- src/ccli.js
+++ src/ccli.js
@@ -1,8 +1,8 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --no-warnings
 /** */
 import {program} from './index.js'
 
 program.name('ccli')
     .description('Templ Container CLI')
-    
+
 program.parseAsync(process.argv)
