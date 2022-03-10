d9efbdeebb84441df7d9cab6df6c7ed58b31c5db
Herman <herman@templ.io>
2022-03-10 17:19:09 +0100

    added version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import {Program} from './utils.js'
 export const program = new Program()
 
+program.command('version').action(async()=>{
+    const p = await import('../package.json',{assert: {type: "json"}})
+    console.log(p.default.version);
+})
+
 /** Top level command which runs sub-commands located in separate files. */
 program
     .command('php','php related commands',  { executableFile: 'php.js' })
