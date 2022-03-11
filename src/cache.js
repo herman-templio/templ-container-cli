edb69853613e7754dee58cc4aafefcb24ef663ff
Herman <herman@templ.io>
2022-03-11 16:56:02 +0100

    added more cache commands

--- src/cache.js
+++ src/cache.js
@@ -14,4 +14,24 @@ cache
         cache.wrap(commandRunner({command:['cache'],args:{cmd:'purge',app:app}}))
     })
 
+cache
+    .command('status [app]')
+    .description('Status of server cache.')
+    .action(async (app)=>{
+        cache.wrap(commandRunner({command:['cache'],args:{cmd:'status',app:app}}))
+    })
+
+cache
+    .command('enable [app]')
+    .description('Enablesserver cache.')
+    .action(async (app)=>{
+        cache.wrap(commandRunner({command:['cache'],args:{cmd:'enable',app:app}}))
+    })
+cache
+    .command('disable [app]')
+    .description('Disables server cache.')
+    .action(async (app)=>{
+        cache.wrap(commandRunner({command:['cache'],args:{cmd:'disable',app:app}}))
+    })
+
 cache.parseAsync(process.argv)
